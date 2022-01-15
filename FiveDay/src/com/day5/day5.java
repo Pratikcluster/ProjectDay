31faaacc611753a076494098a9a9e6ca82d99413
Praticluster <pratikcluster@gmail.com>
2022-01-15 18:58:08 +0530

    Project Day

---
+++ FiveDay/src/com/day5/day5.java
@@ -0,0 +1,7 @@
+package com.day5;
+
+public class day5 {
+    public static void main(String[] args) {
+        System.out.println("Day 5");
+    }
+}
---
+++ FourDay/src/com/day4/day4.java
@@ -0,0 +1,7 @@
+package com.day4;
+
+public class day4 {
+    public static void main(String[] args) {
+        System.out.println("day 4");
+    }
+}
